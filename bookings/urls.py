72e0147838e8195ac9e0d5a98080cd6db17182a4
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2024-06-30 09:22:10 +0100

    trying to fix error on heroku

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,6 @@
 #from django.urls import path
 #from . import views
-#from . import views as booking_views
+#from . import views as booking_views #
 
 # urlpatterns = [
    # path('', views.home, name='home'),
@@ -8,14 +8,13 @@
    # path('view_bookings/', views.view_bookings, name='view_bookings'),
    # path('manage_lessons/', views.manage_lessons, name='manage_lessons'),
    # path('lessons/<int:lesson_id>/', booking_views.lesson_detail, name='lesson_detail'),
-   # path('blog/', booking_views.blog, name='blog'),
-   # path('faq/', booking_views.faq, name='faq'),
    # path('contact/', booking_views.contact, name='contact'),
 #]
 
 
 from django.urls import path
 from . import views
+from . import views as booking_views
 
 urlpatterns = [
     path('', views.home, name='home'),
